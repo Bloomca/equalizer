c328098fcc79b28be8be951db5af1352c676f622
Seva Zaikov <seva.zaikov@gmail.com>
2016-06-30 14:58:20 +0300

    add build configuration

--- webpack.config.js
+++ webpack.config.js
@@ -1,49 +1,70 @@
 const webpack = require('webpack');
 const path = require('path');
 const autoprefixer = require('autoprefixer');
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
 
-module.exports = {
-  // we have to require babel-polyfill first
-  // https://babeljs.io/docs/usage/polyfill/
-  entry: [
-    'babel-polyfill',
-    // For hot style updates
-    'webpack/hot/dev-server',
+module.exports = ({ dev }) => {
+  const devEntries = dev
+    ? [
+      // For hot style updates
+      'webpack/hot/dev-server',
 
-    // The script refreshing the browser on none hot updates
-    'webpack-dev-server/client?http://localhost:8080',
+      // The script refreshing the browser on none hot updates
+      'webpack-dev-server/client?http://localhost:8080'
+    ] : [];
 
-    './src/index.js'
-  ],
-  output: {
-    path: path.resolve(__dirname, 'build'),
-    publicPath: '',
-    filename: 'client.js'
-  },
-  module: {
-    loaders: [
-      {
-        test: /\.js$/,
-        exclude: /(node_modules|bower_components)/,
-        loader: 'babel'
-      },
-      {
-        test:   /\.css$/,
-        loader: 'style-loader!css-loader!postcss-loader'
-      },
-      {
-        test: /\.json$/,
-        loader: 'json'
-      }
-    ]
-  },
-  plugins: [
+  const outputPath = path.resolve(__dirname, dev ? 'build' : 'dist');
+
+  const devPlugins = [
     new webpack.HotModuleReplacementPlugin()
-  ],
-  postcss: [
-    autoprefixer,
-    require('postcss-modules')({
-      generateScopedName: '[name]__[local]___[hash:base64:5]',
-    })
-  ]
+  ];
+
+  const buildPlugins = [
+    new ExtractTextPlugin('styles.css'),
+    new webpack.optimize.DedupePlugin(),
+    new webpack.optimize.UglifyJsPlugin()
+  ];
+
+  const cssLoader = dev
+    ? 'style-loader!css-loader!postcss-loader'
+    : ExtractTextPlugin.extract(
+        'style-loader',
+        'css-loader!postcss-loader');
+  return {
+    // we have to require babel-polyfill first
+    // https://babeljs.io/docs/usage/polyfill/
+    entry: []
+      .concat('babel-polyfill')
+      .concat(devEntries)
+      .concat('./src/index.js'),
+    output: {
+      path: outputPath,
+      publicPath: '',
+      filename: 'client.js'
+    },
+    module: {
+      loaders: [
+        {
+          test: /\.js$/,
+          exclude: /(node_modules|bower_components)/,
+          loader: 'babel'
+        },
+        {
+          test:   /\.css$/,
+          loader: cssLoader
+        },
+        {
+          test: /\.json$/,
+          loader: 'json'
+        }
+      ]
+    },
+    plugins: dev ? devPlugins : buildPlugins,
+    postcss: [
+      autoprefixer,
+      require('postcss-modules')({
+        generateScopedName: '[name]__[local]___[hash:base64:5]',
+      })
+    ]
+  }
 };
