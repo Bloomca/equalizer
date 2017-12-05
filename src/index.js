7a74d1cd2fb1b908ca254924fe8d52e9af59ec3b
Seva Zaikov <seva.zaikov@gmail.com>
2017-12-05 22:26:42 +0100

    update core dependencies

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
-import runApplication from './main';
+import runApplication from "./main";
 
-document.addEventListener('DOMContentLoaded', () => {
+document.addEventListener("DOMContentLoaded", () => {
   SC.initialize({
-    client_id: '129995c68429621b69af9121acc1c116'
+    client_id: "129995c68429621b69af9121acc1c116"
   });
 
-  runApplication('app');
+  runApplication("app");
 });
--- src/main.js
+++ src/main.js
@@ -1,59 +1,61 @@
 // cycle declaration
-import xs from 'xstream';
-import {run} from '@cycle/xstream-run';
+import xs from "xstream";
+import { run } from "@cycle/run";
 
 // drivers declaration
-import {div, label, input, hr, h1, h3, makeDOMDriver} from '@cycle/dom';
-import { playDriver } from './drivers/play';
-import { tracksDriver } from './drivers/tracks';
+import { div, makeDOMDriver } from "@cycle/dom";
+import { playDriver } from "./drivers/play";
+import { tracksDriver } from "./drivers/tracks";
 
 // components declaration
-import Header from './components/header';
-import Footer from './components/footer';
-import Tabs from './components/tabs';
-import Search from './components/search';
-import Tracks from './components/tracks';
-import Controls from './components/controls';
-import Slider from './components/slider';
+import Header from "./components/header";
+import Footer from "./components/footer";
+import Tabs from "./components/tabs";
+import Controls from "./components/controls";
+import Slider from "./components/slider";
 
 // style declaration
-import './common.css';
+import "./common.css";
 
 function main(sources) {
-  const mouseMove$ = sources.DOM.select('.root').events('mousemove');
-  const mouseUp$ = sources.DOM.select('.root').events('mouseup');
+  const mouseMove$ = sources.DOM.select(".root").events("mousemove");
+  const mouseUp$ = sources.DOM.select(".root").events("mouseup");
   const globalEvents = { mouseMove$, mouseUp$ };
 
   const headerComponent = Header();
   const footerComponent = Footer();
   const player$ = sources.player.getState();
-  const tabsComponent = Tabs({ DOM: sources.DOM, player$, data: sources.data, player$ });
-  const searchComponent = Search(sources);
+  const tabsComponent = Tabs({
+    DOM: sources.DOM,
+    player$,
+    data: sources.data
+  });
   const tracks$ = tabsComponent.tracks$;
-  const controlsComponent = Controls({ DOM: sources.DOM, player$, tracks$, play: sources.player.play });
+  const controlsComponent = Controls({
+    DOM: sources.DOM,
+    player$,
+    tracks$,
+    play: sources.player.play
+  });
   const sliderComponent = Slider({ DOM: sources.DOM, globalEvents });
 
   const sinks = {
-    DOM: xs.combine(
-      controlsComponent.DOM,
-      sliderComponent.DOM,
-      headerComponent.DOM,
-      footerComponent.DOM,
-      tabsComponent.DOM
-    ).map(([controlsDOM, sliderDOM, headerDOM, footerDOM, tabsDOM]) => {
-        return div('.root', [
+    DOM: xs
+      .combine(
+        controlsComponent.DOM,
+        sliderComponent.DOM,
+        headerComponent.DOM,
+        footerComponent.DOM,
+        tabsComponent.DOM
+      )
+      .map(([controlsDOM, _sliderDOM, headerDOM, footerDOM, tabsDOM]) => {
+        return div(".root", [
           headerDOM,
-          div('.mui-container', [
-            controlsDOM,
-            tabsDOM
-          ]),
+          div(".mui-container", [controlsDOM, tabsDOM]),
           footerDOM
-        ])
+        ]);
       }),
-    player: xs.merge(
-      tabsComponent.play,
-      controlsComponent.play
-    )
+    player: xs.merge(tabsComponent.play, controlsComponent.play)
   };
   return sinks;
 }
